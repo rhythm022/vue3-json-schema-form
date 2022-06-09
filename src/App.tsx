1517bb3c3e61c57b04c923a7404c012d4f0e756f
jiangjj <jiangjj@sse.net.cn>
2022-06-09 13:49:34 +0800

    chore: init monaco-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
-import { defineComponent } from 'vue'
+/* eslint no-empty: 0 */
+
+import { defineComponent, ref, Ref } from 'vue'
+import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+const schema = {
+  type: 'string',
+}
 export default defineComponent({
   setup() {
-    return () => <div>123</div>
+    const schemaRef: Ref<any> = ref(schema)
+    const handleCodeChange = (code: string) => {
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (err) {}
+      schemaRef.value = schema
+    }
+    const classesRef = useStyles()
+
+    return () => {
+      const code = toJson(schemaRef.value)
+      const classes = classesRef.value
+
+      return (
+        <div>
+          <MonacoEditor
+            class={classes.editor}
+            onChange={handleCodeChange}
+            code={code}
+            title="Schema"
+          />
+        </div>
+      )
+    }
   },
 })
