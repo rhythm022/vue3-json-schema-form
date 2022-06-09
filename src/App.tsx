370690478c5fc174d4625e2d0b12de073f7339c8
jiangjj <jiangjj@sse.net.cn>
2022-06-09 12:21:39 +0800

    chore: clear directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { defineComponent } from 'vue'
-const img = require('./assets/logo.png') // eslint-disable-line
 export default defineComponent({
   setup() {
-    return () => (
-      <div>
-        <img src={img}></img>
-      </div>
-    )
+    return () => <div>123</div>
   },
 })
