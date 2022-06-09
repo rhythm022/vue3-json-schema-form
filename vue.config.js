dde77807bb030c06eac65d707edb94a2c7e09b46
jiangjj <jiangjj@sse.net.cn>
2022-06-09 13:46:20 +0800

    chore: init eslint

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
 module.exports = {
   devServer: {
     open: true,
     hot: true, //自动保存
+  },
+  configureWebpack(config) {
+    // console.log(config.plugins)
+  },
+  chainWebpack(config) {
+    config.plugin('monaco').use(new MonacoWebpackPlugin())
+    // config.plugin('circular').use(new CircularDependencyPlugin())
   },
 }
