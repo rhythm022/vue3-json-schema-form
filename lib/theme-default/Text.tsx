4a78339c931410f4f3e7dc6e62f3686582c0f73f
jiangjj <jiangjj@sse.net.cn>
2022-06-21 16:19:36 +0800

    refactor(text-widget): 受控组件改造

--- lib/theme-default/Text.tsx
+++ lib/theme-default/Text.tsx
@@ -5,7 +5,9 @@ const TextWidget = defineComponent({
   props: CommonWidgetPropsDefine,
   setup(props) {
     const handleChange = (e: any) => {
-      (props as any).onChange(e.target.value)
+      const value = e.target.value
+      e.target.value = props.value // 用 props.value 复原 dom，让 dom 上的 value 只在初始化时被 props 控制。即受控组件。
+      ;(props as any).onChange(value)
     }
     return () => {
       return (
